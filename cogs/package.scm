3c0b65fa8e8a1f1f4e5e207c22415b3bc51d32d6
Tom Waddington <tom@implementor.org>
2025-10-12 12:09:14 +0100

    cleanup

--- cogs/package.scm
+++
@@ -1,8 +0,0 @@
-(define package-name "steel-nrepl")
-(define version "0.1.0")
-(define description "Clojure nREPL client for Helix")
-(define author "Tom Waddington")
-(define dependencies '())
-(define native-dependencies
-  '((platforms ("linux" "darwin" "windows"))
-    (libraries ("libsteel_nrepl"))))
